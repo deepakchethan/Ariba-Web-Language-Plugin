9ec442a5ff0f25741878c4440f66dfd33f22d4f5
Deepak Chethan <deepakchethan@outlook.com>
2022-04-06 15:43:08 +0530

    Supplier completion

--- src/main/kotlin/com/github/deepakchethan/aribaweblanguageplugin/language/highlighter/AWLHighlighterColors.kt
+++ src/main/kotlin/com/github/deepakchethan/aribaweblanguageplugin/language/highlighter/AWLHighlighterColors.kt
@@ -2,12 +2,10 @@ package com.github.deepakchethan.aribaweblanguageplugin.language.highlighter
 
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
 import com.intellij.openapi.editor.HighlighterColors
-import com.intellij.openapi.editor.colors.CodeInsightColors
 import com.intellij.openapi.editor.colors.TextAttributesKey
 
 class AWLHighlighterColors {
     companion object {
-        val AWL_PROLOGUE = TextAttributesKey.createTextAttributesKey("AWL_PROLOGUE", HighlighterColors.TEXT)
         val AWL_COMMENT =
             TextAttributesKey.createTextAttributesKey("AWL_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT)
         val AWL_TAG = TextAttributesKey.createTextAttributesKey("AWL_TAG", DefaultLanguageHighlighterColors.MARKUP_TAG)
@@ -24,8 +22,5 @@ class AWLHighlighterColors {
             DefaultLanguageHighlighterColors.MARKUP_ENTITY
         )
         val AWL_CODE = TextAttributesKey.createTextAttributesKey("AWL_CODE", HighlighterColors.TEXT)
-        val MATCHED_TAG_NAME =
-            TextAttributesKey.createTextAttributesKey("MATCHED_TAG_NAME", CodeInsightColors.MATCHED_BRACE_ATTRIBUTES)
     }
-
 }
